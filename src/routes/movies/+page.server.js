af104deb28af539abc29b6f3827328872759b367
VIGNESH KUMAR <45727291+VICTORVICKIE@users.noreply.github.com>
2023-06-25 12:33:47 +0530

    you can do this

--- src/routes/movies/+page.server.js
+++ src/routes/movies/+page.server.js
@@ -4,12 +4,23 @@ export async function load() {
 	return { result };
 }
 
+
+/**
+ * @param {FormDataEntryValue} input
+ */
+async function you_can_do_this(input) {
+	return input;
+}
+
 /** @type {import('./$types').Actions} */
 export const actions = {
 	movies: async ({ request }) => {
 		let form_data = await request.formData();
+
 		const { prompt } = Object.fromEntries(form_data);
 		console.log(prompt);
+
+		console.log(await you_can_do_this(prompt));
 
 		return { prompt };
 	}
